298440b7a04aa7226ef49f16066ad5d5fac21060
Ryan <ryanpvoong@gmail.com>
2024-08-13 16:29:57 -0400

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div className='text-2xl bg-blue-500'>Hello</div>
+    </div>
+  )
+}
+
+export default App
