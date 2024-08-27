e108740f2dd5290dabd4b36cf578899bb94a130a
Ryan <ryanpvoong@gmail.com>
2024-08-27 19:03:41 -0400

    add functionality to post and receive audio to server

--- client/src/components/Controller.tsx
+++ client/src/components/Controller.tsx
@@ -1,19 +1,59 @@
 import { useState } from 'react';
 import Title from './Title';
 import RecordMessage from './RecordMessage';
+import axios from 'axios';
 
 function Controller() {
   const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState<any[]>([]);
 
   const createBlobUrl = (data: any) => {
+    const blob = new Blob([data], { type: "audio/mpeg" });
+    const url = window.URL.createObjectURL(blob);
+    return url;
+  };
 
-    const handleStop = async() => {
-
-    };
-
-
-  }
+  const handleStop = async(blobUrl: string) => {
+    try {
+      setLoading(true);
+  
+      // append recorded message to messages array
+      const myMessage = { sender: "me", blobUrl };
+      const messagesArr = [...messages, myMessage];
+  
+      // convert blob url to blob object
+      fetch(blobUrl)
+        .then((res) => res.blob())
+        .then(async (blob) => {
+          const formData = new FormData();
+          formData.append("file", blob, "myFile.wav");
+  
+          // Send form data to server
+          await axios.post("http://localhost:8000/audio", formData, { 
+            headers: { "Content-Type": "audio/mpeg" },
+            responseType: "arraybuffer" 
+          }).then((res: any) => {
+            const blob = res.data
+            const audio = new Audio();
+            audio.src = createBlobUrl(blob);
+  
+            const botMessage = { sender: "bot", blobUrl: audio.src };
+            messagesArr.push(botMessage);
+  
+            setMessages(messagesArr);
+          }).catch((err) => {
+            console.log(err);
+          })
+  
+        });
+  
+  
+      } catch (error) {
+        console.log("RECORDING ERROR: ", error);
+      } finally {
+        setLoading(false);
+    }
+  };
 
 
   return (
@@ -26,7 +66,7 @@ function Controller() {
         {/* Recorder  */}
         <div className='fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500'>
           <div className='flex justify-center items-center w-full'>
-            <RecordMessage />
+            <RecordMessage handleStop={ handleStop } />
           </div>
         </div>
       </div>
--- client/src/components/RecordMessage.tsx
+++ client/src/components/RecordMessage.tsx
@@ -19,7 +19,9 @@ function RecordMessage({ handleStop }: Props) {
               onMouseUp={ stopRecording }
               className="bg-white p-4 rounded-full shadow-lg"
             >
-                ICON
+              <RecordIcon 
+                classText={ status === "recording" ? "animate-pulse text-red-500" : "text-sky-500" } 
+              />
             </button>
             <p className="mt-2 text-white font-light">{ status }</p>
           </div> 
